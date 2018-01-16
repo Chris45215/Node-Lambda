c333cd3b205f155ae8dde6a9c6275f110bb01163
Chris45215 <chris45215@gmail.com>
2018-01-15 20:18:42 -0500

    updated index.json to be as simple as possible

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
 const http = require('http');
-const hostname = '127.0.0.1';
+
 const port = 3000;
-const fs = require('fs');
 
-fs.readFile('index2.html', (err, html) => {
-	if (err){
-		throw err;
-	}
-	
-	const server = http.createServer((req, res) => {
-		res.statusCode = 200;
-		res.setHeader('Content-type', 'text/html');
-		res.write(html);
-		res.end('Hello World!');
-	});
-
-	server.listen(port, /*hostname,*/ () => {
-		console.log('Server started on port ' +port);
-	});
-});
-
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => {
+  res.send('HEY!')
+})
+app.listen(port, () => console.log('Server running on port 3000'))
